482b1d0a238f6f9cbb6e073d58bede72d6ae3bbd
SON TRUONG NGUYEN <t.nguyen@innopolis.university>
2021-06-08 15:30:47 +0300

    Visualization updated

--- visualize.py
+++ visualize.py
@@ -1,4 +1,4 @@
-# python visualize.py true/false to save video or not
+# python visualize.py filename true/false  (true/false is to save or not)
 
 from collections import Counter
 import pandas as pd
@@ -15,7 +15,7 @@ def animate_scatters(i, refine_data, scatters):
             scatters[j]._offsets3d = (refine_data[i][j][0:1], refine_data[i][j][1:2], refine_data[i][j][2:])
         return scatters
     except Exception:
-        print("Error with i= ", i, "and j= ", j)
+        print("Error with timestamp ", i, "and marker number ", j)
 
 
 def unique(sequence):
@@ -24,66 +24,71 @@ def unique(sequence):
 
 
 # Read data
-data = pd.read_csv('Thursday.csv')
+data = pd.read_csv(argv[1])
 
 # Classify data into categories
 timestamp = data["time"]
 timestamp_unique = unique(timestamp)
 id = data["id"]
-num_marker = len(set(id))
-num_iter = len(timestamp_unique)
+num_iter = len(set(timestamp))
 x = data["x"]
-y = data["y"]
-z = data["z"]
+z = data["y"]
+y = data["z"]
 
 num_marker_per_timestamp = Counter(timestamp)
 
 # Cluster it by timestamp, debug and check the inner structure, it's easy.
 # Timestamp x marker x coordinates
-refine_data = [0 for _ in range(len(timestamp_unique))]
+refine_data = []
 
-k = 0
-i = 0
+i = 0  # pointer to each duplicated timestamp
 
 while i < len(timestamp):
-    refine_data[k] = [[x[i + j], y[i + j], z[i + j]] for j in range(num_marker_per_timestamp[timestamp_unique[k]])]
-    i += len(refine_data[k])
-    k += 1
+    num_marker = num_marker_per_timestamp[timestamp[i]]
+    refine_data.append([[x[i + j], y[i + j], z[i + j]] for j in range(num_marker)])
+    i += num_marker
 
 fig = plt.figure()
 ax = p3.Axes3D(fig)
 
 # Setting the axes properties
-ax.set_xlim3d([-2, 2])
+ax.set_xlim3d([-2*max(abs(x.min()), abs(x.max())), 2*max(abs(x.min()), abs(x.max()))])
 ax.set_xlabel('X')
 
-ax.set_ylim3d([-2, 2])
+ax.set_ylim3d([-2*max(abs(y.min()), abs(y.max())), 2*max(abs(y.min()), abs(y.max()))])
 ax.set_ylabel('Y')
 
-ax.set_zlim3d([-2, 2])
+# ax.set_zlim3d([0, max(abs(z.min()), abs(z.max()))])
+ax.set_zlim3d([-2*max(abs(z.min()), abs(z.max())), 2*max(abs(z.min()), abs(z.max()))])
 ax.set_zlabel('Z')
 
 ax.set_title('3D visualization')
 
 # Provide starting angle for the view.
-ax.view_init(25, 10)
+ax.view_init(0, -40)
 
 if __name__ == '__main__':
-    assert argv[1].lower() == "true" or argv[1].lower() == "false"
-    if argv[1].lower() == "true":
+
+    # save setting:
+    assert argv[2].lower() == "true" or argv[2].lower() == "false"
+    if argv[2].lower() == "true":
         save = True
     else:
         save = False
+
     # Initialize scatters
-    scatters = [ax.scatter(x[i].reshape(1, 1), y[i].reshape(1, 1), z[i].reshape(1, 1)) for i in
-                range(num_marker_per_timestamp[timestamp_unique[0]])]
+    scatters = [ax.scatter(x[i].reshape(1, 1), y[i].reshape(1, 1), z[i].reshape(1, 1), c='black', s=1, marker='x') for i in
+                range(num_marker_per_timestamp[timestamp[0]])]
+
+    for i in range(num_marker_per_timestamp[timestamp[0]]):
+        scatters[i].set
 
     ani = animation.FuncAnimation(fig, animate_scatters, num_iter, fargs=(refine_data, scatters),
-                                  interval=1, blit=False, repeat=True)
+                                  interval=-0.1, blit=False, repeat=True)
 
     if save:
         Writer = animation.writers['ffmpeg']
-        writer = Writer(fps=30, metadata=dict(artist='Me'), bitrate=1800, extra_args=['-vcodec', 'libx264'])
+        writer = Writer(fps=30, metadata=dict(artist='Me'), bitrate=4000, extra_args=['-vcodec', 'libx264'])
         ani.save('3d-scatted-animated.mp4', writer=writer)
 
-    plt.show()
+    # plt.show()

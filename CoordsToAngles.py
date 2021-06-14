6b367a97eed8a2235b304f7e195c5da4524f3de2
Ignat <petrov-ignat85@mail.ru>
2021-06-14 13:34:39 +0300

    updated angles

--- CoordsToAngles.py
+++ CoordsToAngles.py
@@ -14,29 +14,82 @@ class Vector:
 # Constants
 PATH_COORDS = r"Coords/"
 PATH_ANGLES = r"Angles/"
-HEADER = ["time", "leg left", "leg right", "arm left", "arm right"]
+HEADER = ["time", "leg left", "leg right", "arm left", "arm right", "back",
+          "left leg/lower back", "right leg/lower back", "left arm/upper back", "right arm/upper back",
+          "left leg/back", "right leg/back", "left arm/back", "right arm/back"]
 
 files_in_coord = os.listdir(PATH_COORDS)
 files_in_angles = os.listdir(PATH_ANGLES)
 
 
+def get_angle_between_bones(bone1, bone2):
+    if bone1 is None or bone2 is None:
+        return 0
+
+    len_bone1 = math.sqrt(bone1.x ** 2 + bone1.y ** 2 + bone1.z ** 2)
+    len_bone2 = math.sqrt(bone2.x ** 2 + bone2.y ** 2 + bone2.z ** 2)
+
+    cos = (bone1.x * bone2.x + bone1.y * bone2.y + bone1.z * bone2.z) / (len_bone1 * len_bone2)
+
+    return math.degrees(math.acos(cos))
+
+
 def make_angles(markers_data, description):
+    bones = []
     angles = []
     for body_part in description:
-        marker1 = markers_data[body_part[0]]
-        marker2 = markers_data[body_part[1]]
-        marker3 = markers_data[body_part[2]]
-        marker4 = markers_data[body_part[3]]
+        marker1 = markers_data[body_part[0]] if body_part[0] in markers_data else None
+        marker2 = markers_data[body_part[1]] if body_part[1] in markers_data else None
+        marker3 = markers_data[body_part[2]] if body_part[2] in markers_data else None
+        marker4 = markers_data[body_part[3]] if body_part[3] in markers_data else None
 
-        bone1 = Vector(marker2.x - marker1.x, marker2.y - marker1.y, marker2.z - marker1.z)
-        bone2 = Vector(marker3.x - marker4.x, marker3.y - marker4.y, marker3.z - marker4.z)
+        bone1 = None
+        bone2 = None
+        if marker1 is not None and marker2 is not None:
+            bone1 = Vector(marker2.x - marker1.x, marker2.y - marker1.y, marker2.z - marker1.z)
+        if marker3 is not None and marker4 is not None:
+            bone2 = Vector(marker3.x - marker4.x, marker3.y - marker4.y, marker3.z - marker4.z)
 
-        len_bone1 = math.sqrt(bone1.x ** 2 + bone1.y ** 2 + bone1.z ** 2)
-        len_bone2 = math.sqrt(bone2.x ** 2 + bone2.y ** 2 + bone2.z ** 2)
+        angles.append(get_angle_between_bones(bone1, bone2))
+        bones.append(bone1)
+        bones.append(bone2)
 
-        cos = (bone1.x * bone2.x + bone1.y * bone2.y + bone1.z * bone2.z) / (len_bone1 * len_bone2)
+        # hole back
+        if len(bones) == 10:
+            back1 = None
+            back2 = None
+            if marker1 is not None and marker4 is not None:
+                back1 = Vector(marker4.x - marker1.x, marker4.y - marker1.y, marker4.z - marker1.z)
+                back2 = Vector(marker1.x - marker4.x, marker1.y - marker4.y, marker1.z - marker4.z)
+            bones.append(back1)
+            bones.append(back2)
 
-        angles.append(math.degrees(math.acos(cos)))
+    # angles between:
+
+    # lower back and left leg
+    angles.append(get_angle_between_bones(bones[1], bones[8]))
+
+    # lower back and right leg
+    angles.append(get_angle_between_bones(bones[3], bones[8]))
+
+    # upper back and left arm
+    angles.append(get_angle_between_bones(bones[4], bones[9]))
+
+    # upper back and right arm
+    angles.append(get_angle_between_bones(bones[6], bones[9]))
+
+    # hole back and left leg
+    angles.append(get_angle_between_bones(bones[1], bones[10]))
+
+    # hole back and right leg
+    angles.append(get_angle_between_bones(bones[3], bones[10]))
+
+    # hole back and left arm
+    angles.append(get_angle_between_bones(bones[4], bones[11]))
+
+    # hole back and right arm
+    angles.append(get_angle_between_bones(bones[6], bones[11]))
+
     return angles
 
 

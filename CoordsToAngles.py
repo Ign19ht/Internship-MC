95c7f7f1351f2ea39d934c0ed6c66bdfa9e165cb
Ignat <petrov-ignat85@mail.ru>
2021-06-24 23:41:12 +0300

    rewrote CoordsToAngles.py to OOP and added 2 angles

--- CoordsToAngles.py
+++ CoordsToAngles.py
@@ -1,157 +1,140 @@
 import csv
 import math
 import os
-from dataclasses import dataclass
-
-
-@dataclass
-class Vector:
-    x: float
-    y: float
-    z: float
+from MarkerDataTypes import Vector
+from myCSV import myCSV, HeaderType
 
 
 # Constants
 PATH_COORDS = r"Coords/"
 PATH_ANGLES = r"Angles/"
-HEADER = ["time", "leg left", "leg right", "arm left", "arm right", "back",
-          "left leg/lower back", "right leg/lower back", "left arm/upper back", "right arm/upper back",
-          "left leg/back", "right leg/back", "left arm/back", "right arm/back"]
-
-files_in_coord = os.listdir(PATH_COORDS)
-files_in_angles = os.listdir(PATH_ANGLES)
 
 
-def get_angle_between_bones(bone1, bone2):
-    if bone1 is None or bone2 is None:
-        return 0
+class TranslateToAngles:
 
-    len_bone1 = math.sqrt(bone1.x ** 2 + bone1.y ** 2 + bone1.z ** 2)
-    len_bone2 = math.sqrt(bone2.x ** 2 + bone2.y ** 2 + bone2.z ** 2)
+    def __init__(self):
+        self.files_in_coord = os.listdir(PATH_COORDS)
+        self.files_in_angles = os.listdir(PATH_ANGLES)
 
-    cos = (bone1.x * bone2.x + bone1.y * bone2.y + bone1.z * bone2.z) / (len_bone1 * len_bone2)
+    @staticmethod
+    def get_angle_between_bones(bone1, bone2):
+        if bone1 is None or bone2 is None:
+            return 0
 
-    return math.degrees(math.acos(cos))
+        len_bone1 = math.sqrt(bone1.x ** 2 + bone1.y ** 2 + bone1.z ** 2)
+        len_bone2 = math.sqrt(bone2.x ** 2 + bone2.y ** 2 + bone2.z ** 2)
 
+        cos = (bone1.x * bone2.x + bone1.y * bone2.y + bone1.z * bone2.z) / (len_bone1 * len_bone2)
 
-def make_angles(markers_data, description):
-    bones = []
-    angles = []
-    for body_part in description:
-        marker1 = markers_data[body_part[0]] if body_part[0] in markers_data else None
-        marker2 = markers_data[body_part[1]] if body_part[1] in markers_data else None
-        marker3 = markers_data[body_part[2]] if body_part[2] in markers_data else None
-        marker4 = markers_data[body_part[3]] if body_part[3] in markers_data else None
+        return math.degrees(math.acos(cos))
 
-        bone1 = None
-        bone2 = None
-        if marker1 is not None and marker2 is not None:
-            bone1 = Vector(marker2.x - marker1.x, marker2.y - marker1.y, marker2.z - marker1.z)
-        if marker3 is not None and marker4 is not None:
-            bone2 = Vector(marker3.x - marker4.x, marker3.y - marker4.y, marker3.z - marker4.z)
+    @staticmethod
+    def make_angles(markers_data, description):
+        bones = []
+        angles = []
+        for body_part in description:
+            marker1 = markers_data[body_part[0]] if body_part[0] in markers_data else None
+            marker2 = markers_data[body_part[1]] if body_part[1] in markers_data else None
+            marker3 = markers_data[body_part[2]] if body_part[2] in markers_data else None
+            marker4 = markers_data[body_part[3]] if body_part[3] in markers_data else None
 
-        angles.append(get_angle_between_bones(bone1, bone2))
-        bones.append(bone1)
-        bones.append(bone2)
+            bone1 = None
+            bone2 = None
+            if marker1 is not None and marker2 is not None:
+                bone1 = Vector(marker2.x - marker1.x, marker2.y - marker1.y, marker2.z - marker1.z)
+            if marker3 is not None and marker4 is not None:
+                bone2 = Vector(marker3.x - marker4.x, marker3.y - marker4.y, marker3.z - marker4.z)
 
-        # hole back
-        if len(bones) == 10:
-            back1 = None
-            back2 = None
-            if marker1 is not None and marker4 is not None:
-                back1 = Vector(marker4.x - marker1.x, marker4.y - marker1.y, marker4.z - marker1.z)
-                back2 = Vector(marker1.x - marker4.x, marker1.y - marker4.y, marker1.z - marker4.z)
-            bones.append(back1)
-            bones.append(back2)
+            angles.append(TranslateToAngles.get_angle_between_bones(bone1, bone2))
+            bones.append(bone1)
+            bones.append(bone2)
 
-    # angles between:
+            # hole back
+            if len(bones) == 10:
+                back1 = None
+                back2 = None
+                if marker1 is not None and marker4 is not None:
+                    back1 = Vector(marker4.x - marker1.x, marker4.y - marker1.y, marker4.z - marker1.z)
+                    back2 = Vector(marker1.x - marker4.x, marker1.y - marker4.y, marker1.z - marker4.z)
+                bones.append(back1)
+                bones.append(back2)
 
-    # lower back and left leg
-    angles.append(get_angle_between_bones(bones[1], bones[8]))
+        # angles between:
+        # floor and left leg
+        angles.append(TranslateToAngles.get_angle_between_bones(bones[0], Vector(bones[0].x, bones[0].y, 0)))
 
-    # lower back and right leg
-    angles.append(get_angle_between_bones(bones[3], bones[8]))
+        # floor and right leg
+        angles.append(TranslateToAngles.get_angle_between_bones(bones[2], Vector(bones[2].x, bones[2].y, 2)))
 
-    # upper back and left arm
-    angles.append(get_angle_between_bones(bones[4], bones[9]))
+        # lower back and left leg
+        angles.append(TranslateToAngles.get_angle_between_bones(bones[1], bones[8]))
 
-    # upper back and right arm
-    angles.append(get_angle_between_bones(bones[6], bones[9]))
+        # lower back and right leg
+        angles.append(TranslateToAngles.get_angle_between_bones(bones[3], bones[8]))
 
-    # hole back and left leg
-    angles.append(get_angle_between_bones(bones[1], bones[10]))
+        # upper back and left arm
+        angles.append(TranslateToAngles.get_angle_between_bones(bones[4], bones[9]))
 
-    # hole back and right leg
-    angles.append(get_angle_between_bones(bones[3], bones[10]))
+        # upper back and right arm
+        angles.append(TranslateToAngles.get_angle_between_bones(bones[6], bones[9]))
 
-    # hole back and left arm
-    angles.append(get_angle_between_bones(bones[4], bones[11]))
+        # hole back and left leg
+        angles.append(TranslateToAngles.get_angle_between_bones(bones[1], bones[10]))
 
-    # hole back and right arm
-    angles.append(get_angle_between_bones(bones[6], bones[11]))
+        # hole back and right leg
+        angles.append(TranslateToAngles.get_angle_between_bones(bones[3], bones[10]))
 
-    return angles
+        # hole back and left arm
+        angles.append(TranslateToAngles.get_angle_between_bones(bones[4], bones[11]))
 
+        # hole back and right arm
+        angles.append(TranslateToAngles.get_angle_between_bones(bones[6], bones[11]))
 
-def save_angles(package_name, file_name, angles, time):
-    with open(PATH_ANGLES + package_name + r"/" + file_name, 'a', newline='') as csv_file:
-        writer = csv.writer(csv_file)
-        writer.writerow([time] + angles)
+        return angles
 
+    def translate_all(self):
+        for package in self.files_in_coord:
+            print(package + ":")
 
-def create_angles_file(package_name, file_name):
-    with open(PATH_ANGLES + package_name + r"/" + file_name, 'w', newline='') as csv_file:
-        writer = csv.writer(csv_file)
-        writer.writerow(HEADER)
-
-
-def check_coords_package():
-    for package in files_in_coord:
-        print(package + ":")
-
-        # check for exist
-        if package in files_in_angles:
-            print("has already been done")
-            continue
-        else:
-            os.mkdir(PATH_ANGLES + package)
-
-        # get files
-        files = os.listdir(PATH_COORDS + package)
-
-        # initialize description
-        description = [[0 for _ in range(4)] for _ in range(5)]
-
-        # get description
-        with open(PATH_COORDS + package + r"/BP.csv") as csv_file:
-            reader = csv.DictReader(csv_file)
-            for row in reader:
-                f = int(row["body part"]) // 10 - 1
-                l = int(row["body part"]) % 10 - 1
-                description[f][l] = row["id"]
-
-        # translation files
-        for file in files:
-            if file == "BP.csv":
+            # check for exist
+            if package in self.files_in_angles:
+                print("has already been done")
                 continue
+            else:
+                os.mkdir(PATH_ANGLES + package)
 
-            print(file + ":", end=" ")
+            # get files
+            files = os.listdir(PATH_COORDS + package)
 
-            create_angles_file(package, file)
+            # get description
+            temp_description = myCSV.get_description(PATH_COORDS + package)
+            description = [[0 for _ in range(4)] for _ in range(5)]
+            for bp, id in temp_description:
+                description[bp // 10 - 1][bp % 10 - 1] = row["id"]
 
-            with open(PATH_COORDS + package + r"/" + file) as csv_file:
-                reader = csv.DictReader(csv_file)
-                current_time = "0.0"
-                markers_data = dict()
-                for row in reader:
-                    if current_time != row["time"]:
-                        save_angles(package, file, make_angles(markers_data, description), current_time)
-                        current_time = row["time"]
-                        markers_data.clear()
-                    markers_data[row["id"]] = Vector(float(row["x"]), float(row["y"]), float(row["z"]))
-                save_angles(package, file, make_angles(markers_data, description), current_time)
+            # translation files
+            for file in files:
+                if file == "BP.csv":
+                    continue
 
-            print("done")
+                print(file + ":", end=" ")
+
+                my_csv = myCSV(PATH_COORDS + package + r"/" + package)
+                my_csv.create_file(file, HeaderType.ANGLES)
+
+                with open(PATH_COORDS + package + r"/" + file) as csv_file:
+                    reader = csv.DictReader(csv_file)
+                    current_time = "0.0"
+                    markers_data = dict()
+                    for row in reader:
+                        if current_time != row["time"]:
+                            my_csv.save_angles(file, self.make_angles(markers_data, description), current_time)
+                            current_time = row["time"]
+                            markers_data.clear()
+                        markers_data[row["id"]] = Vector(float(row["x"]), float(row["y"]), float(row["z"]))
+                    my_csv.save_angles(file, self.make_angles(markers_data, description), current_time)
+
+                print("done")
 
 
 if __name__ == '__main__':
@@ -159,4 +142,5 @@ if __name__ == '__main__':
     if not os.path.exists(PATH_ANGLES):
         os.mkdir(PATH_ANGLES)
 
-    check_coords_package()
+    translator = TranslateToAngles()
+    translator.translate_all()

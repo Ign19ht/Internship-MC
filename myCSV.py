cc8bb4953adf37f68537214453136eb29d13f19e
Ignat <petrov-ignat85@mail.ru>
2021-06-24 22:28:16 +0300

    little changes

--- myCSV.py
+++ myCSV.py
@@ -7,13 +7,15 @@ class HeaderType(Enum):
     ANGLES = 1
 
 
+HEADERS = [["time", "id", "x", "y", "z"],
+           ["time", "leg left", "leg right", "arm left", "arm right", "back",
+            "left leg/lower back", "right leg/lower back", "left arm/upper back", "right arm/upper back",
+            "left leg/back", "right leg/back", "left arm/back", "right arm/back"]]
+
+
 class myCSV:
     def __init__(self, path):
         self.path = path
-        self.headers = [["time", "id", "x", "y", "z"],
-                       ["time", "leg left", "leg right", "arm left", "arm right", "back",
-                        "left leg/lower back", "right leg/lower back", "left arm/upper back", "right arm/upper back",
-                        "left leg/back", "right leg/back", "left arm/back", "right arm/back"]]
 
     def save_markers(self, file_name, markers_data, time):
         with open(self.path + r"/" + file_name, 'a', newline='') as csv_file:
@@ -26,9 +28,9 @@ class myCSV:
             writer = csv.writer(csv_file)
             writer.writerow([time] + angles)
 
-    def create_file(self, file_name, type:HeaderType):
+    def create_file(self, file_name, type: HeaderType):
         with open(self.path + r"/" + file_name, 'w', newline='') as csv_file:
-            csv.writer(csv_file).writerow(self.headers[type])
+            csv.writer(csv_file).writerow(HEADERS[type.value])
 
     @staticmethod
     def get_description(path):
@@ -40,7 +42,7 @@ class myCSV:
         return description
 
     def save_bp(self, description: dict):
-        with open(self.path + "BP.csv", 'w', newline='') as csvfile:
+        with open(self.path + "/BP.csv", 'w', newline='') as csvfile:
             writer = csv.writer(csvfile)
             writer.writerow(["id", "body part"])
             for bp, id in description.items():
